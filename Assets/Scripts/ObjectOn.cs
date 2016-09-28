3028ff354e3a2100ffe2dc51bdca89539406d8d3
Kristian Gaedt <kristian.gaedt@ad.aie.edu>
2016-09-28 11:37:17 +0930

    Script for toggling objects

---
+++ Assets/Scripts/ObjectOn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectOn : MonoBehaviour {
+
+    public GameObject Object1;
+    bool objectToggle;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            objectToggle = !objectToggle;
+            Object1.SetActive(objectToggle);
+        }
+    }
+}
