b26c495bb48c43db0e0945956cc475aa388eb193
Kristian Gaedt <kristian.gaedt@ad.aie.edu>
2016-10-18 11:10:46 +1030

    Cave level building 3/2

---
+++ Assets/Scripts/HallwaySwitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HallwaySwitch : MonoBehaviour {
+
+    public GameObject Object1;
+    public GameObject Object2;
+    public GameObject Object3;
+    public GameObject Object4;
+    public GameObject triggerOff;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Object1.SetActive(true);
+        Object2.SetActive(true);
+        Object3.SetActive(false);
+        Object4.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        triggerOff.SetActive(false);
+    }
+}
