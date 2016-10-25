2eb533aced681ce693ebbb8888a9f0136771bf7a
Kristian Gaedt <kristian.gaedt@ad.aie.edu>
2016-10-25 16:25:07 +1030

    Fixed a few assets, added textures, etc.

---
+++ Assets/Scripts/HallwaySwitchOne.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HallwaySwitchOne : MonoBehaviour {
+
+    public GameObject Object_On;
+    public GameObject Object_Off;
+    public GameObject triggerOff;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Object_On.SetActive(true);
+        Object_Off.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        triggerOff.SetActive(false);
+    }
+}
