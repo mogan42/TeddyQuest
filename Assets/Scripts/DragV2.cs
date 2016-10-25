9d19a58cf21d45882c724dfda7b76eaa10ccc5bf
AIE\jacob.will <coba17@mail.com>
2016-10-25 11:37:17 +1030

    started Ai

--- Assets/Scripts/DragV2.cs
+++ Assets/Scripts/DragV2.cs
@@ -1,85 +1,45 @@
-﻿//using UnityEngine;
-//using System.Collections;
-
-//public class DragV2 : MonoBehaviour
-//{
-//    public GameObject player;
-//    public float heightOfTheGround = 0.5f;
-//    public float lowToGround = 0.5f;
-//    // Use this for initialization
-//    void Start ()
-//    {
-	
-//	}
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//            RaycastHit hit;
-//            //checks to see if you are clicking on an object
-//            if (Physics.Raycast(ray, out hit))
-//            {
-//                if (hit.transform.tag == "box")
-//                {
-//                    hit.transform.parent = player;
-//                    if (hit.transform.position.y > heightOfTheGround)
-//                    {
-//                        hit.transform.position = new Vector3(hit.transform.position.x, heightOfTheGround - 0.001f, hit.transform.position.z);
-//                    }
-//                    if (hit.transform.position.y < lowToGround)
-//                    {
-//                        hit.transform.position = new Vector3(hit.transform.position.x, lowToGround + 0.001f, hit.transform.position.z);
-//                    }
-//                }
-
-//            }
-//        }
-//    }
-//}
+﻿
+using UnityEngine;
+using System.Collections;
 
 
-//original VVVV
+public class DragV2 : MonoBehaviour
+{
+    public GameObject other;
+    float distance;
+    public float distanceFromObject = 10;
+    public float heightOfTheGround = 6.5f;
+    public float lowToGround = 0.2f;
+    Vector3 startingPosition;
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, other.transform.position);
+        startingPosition = transform.position;
+    }
 
+    void OnMouseDrag()
+    {
+        if (distance < distanceFromObject)
+        {
+            int x = Screen.width / 2;
+            int y = Screen.width / 2;
+            Vector3 mousePosition = new Vector3(x, y, distance);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Vector3 playerToObject = objPosition - other.transform.position;
+            playerToObject.Normalize();
+            playerToObject *= distance;
+            transform.position = other.transform.position + playerToObject;
+           // other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            if (transform.position.y > heightOfTheGround)
+            {
+                transform.position = new Vector3(transform.position.x, heightOfTheGround - 0.001f, transform.position.z);
+            }
+            if (transform.position.y < lowToGround)
+            {
+                transform.position = new Vector3(transform.position.x, lowToGround + 0.001f, transform.position.z);
+            }
+        }
 
-//using UnityEngine;
-//using System.Collections;
-
-
-//public class Drag : MonoBehaviour
-//{
-//    public Transform other;
-//    float distance;
-//    public float distanceFromObject = 10;
-//    public float heightOfTheGround = 0.5f;
-//    public float lowToGround = 0.5f;
-//    void OnMouseDown()
-//    {
-//        distance = Vector3.Distance(transform.position, other.transform.position);
-//    }
-
-//    void OnMouseDrag()
-//    {
-//        if (distance < distanceFromObject)
-//        {
-//            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-//            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-//            Vector3 playerToObject = objPosition - other.transform.position;
-//            playerToObject.Normalize();
-//            playerToObject *= distance;
-//            transform.position = other.transform.position + playerToObject;
-//            if (transform.position.y > heightOfTheGround)
-//            {
-//                transform.position = new Vector3(transform.position.x, heightOfTheGround - 0.001f, transform.position.z);
-//            }
-//            if (transform.position.y < lowToGround)
-//            {
-//                transform.position = new Vector3(transform.position.x, lowToGround + 0.001f, transform.position.z);
-//            }
-//        }
-
-//    }
-//}
+    }
+}
 
---
+++ Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderingAI : MonoBehaviour
+{
+    /*check if player is within range.
+    if player is run straight to them, 
+    else wander(get a
+    */
+    public float searchRadius= 50;
+    public GameObject player;
+    private NavMeshAgent m_agent;
+    bool chaseable = false;
+    float distanceFromPlayer;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceFromPlayer <= searchRadius)
+        {
+            chaseable = true;
+            Debug.Log("player is in range");
+        }
+        else if (distanceFromPlayer > searchRadius)
+        {
+            chaseable = false;
+            Debug.Log("player is not in range");
+        }
+        if (chaseable)
+        {
+            Chase();
+        }
+        else if (!chaseable)
+        {
+            Wander();
+        }
+
+    }
+
+    void Chase()
+    {
+        m_agent.destination = player.transform.position;
+    }
+    void Wander()
+    {
+        Debug.Log("I need to wander");
+    }
+}
