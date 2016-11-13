e6462d0a31fc67a855c7c02acf52077640a11289
Morgans-PC\Morgan <mo.g@live.com.au>
2016-11-13 22:23:32 +1030

    still smashing my head on this code

--- Assets/Scripts/LevelExits.cs
+++ Assets/Scripts/LevelExits.cs
@@ -7,49 +7,89 @@ public class LevelExits : MonoBehaviour {
     public GameObject amazonLibrary;
     public GameObject childsRoom;
 
+    private GameObject levelEntrancePoint;
     public GameObject globalController;
-
-	// Use this for initialization
-	void Start () {
-        GlobalLog levelEntrancePoint = globalController.GetComponent<GlobalLog>();
-        //Debug.Log("ahhhh");
-	    if (levelEntrancePoint.caveExit == true)
-            {
-            caveLevel.SetActive(true);
-            vegForest.SetActive(false);
-            amazonLibrary.SetActive(false);
-            childsRoom.SetActive(false);
-            }
-
-        else if (levelEntrancePoint.vegForestExit == true)
-            {
-            caveLevel.SetActive(false);
-            vegForest.SetActive(true);
-            amazonLibrary.SetActive(false);
-            childsRoom.SetActive(false);
-            }
-
-        else if (levelEntrancePoint.amazonLibraryExit == true)
-            {
-            caveLevel.SetActive(false);
-            vegForest.SetActive(false);
-            amazonLibrary.SetActive(true);
-            childsRoom.SetActive(false);
-            }
-
-        else if (levelEntrancePoint.childsRoomExit == true)
-            {
-            Debug.Log("this works");
-            caveLevel.SetActive(false);
-            vegForest.SetActive(false);
-            amazonLibrary.SetActive(false);
-            childsRoom.SetActive(true);
-            }
+    // Use this for initialization
+    void Start()
+    {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    /*if (levelEntrancePoint.vegForestExit == true)
+         {
+         Debug.Log("second level down");
+         caveLevel.SetActive(false);
+         vegForest.SetActive(true);
+         amazonLibrary.SetActive(false);
+         childsRoom.SetActive(false);
+         }
+
+     if (levelEntrancePoint.amazonLibraryExit == true)
+         {
+         Debug.Log("Thrid level down");
+         caveLevel.SetActive(false);
+         vegForest.SetActive(false);
+         amazonLibrary.SetActive(true);
+         childsRoom.SetActive(false);
+         }
+
+     if (levelEntrancePoint.childsRoomExit == true)
+         {
+         Debug.Log("Forth level down");
+         caveLevel.SetActive(false);
+         vegForest.SetActive(false);
+         amazonLibrary.SetActive(false);
+         childsRoom.SetActive(true);
+         }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GlobalLog levelEntrancePoint = globalController.GetComponent<GlobalLog>();
+        //levelEntrancePoint = GameObject.Find("GlobalLogObject").GetComponent<GlobalLog>();
+        //{
+            //Debug.Log("ahhhh");
+            if (levelEntrancePoint.caveExit == true)
+            {
+                Debug.Log("First level down");
+                caveLevel.SetActive(true);
+                vegForest.SetActive(false);
+                amazonLibrary.SetActive(false);
+                childsRoom.SetActive(false);
+
+                if (levelEntrancePoint.vegForestExit == true)
+                {
+                    Debug.Log("second level down");
+                    caveLevel.SetActive(false);
+                    vegForest.SetActive(true);
+                    amazonLibrary.SetActive(false);
+                    childsRoom.SetActive(false);
+
+                    if (levelEntrancePoint.amazonLibraryExit == true)
+                    {
+                        Debug.Log("Thrid level down");
+                        caveLevel.SetActive(false);
+                        vegForest.SetActive(false);
+                        amazonLibrary.SetActive(true);
+                        childsRoom.SetActive(false);
+
+                        if (levelEntrancePoint.childsRoomExit == true)
+                        {
+                            Debug.Log("Forth level down");
+                            caveLevel.SetActive(false);
+                            vegForest.SetActive(false);
+                            amazonLibrary.SetActive(false);
+                            childsRoom.SetActive(true);
+                        }
+
+                    }
+
+                }
+
+            }
+
+        //}
+    }
+
 }
