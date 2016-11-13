d065852d7bb6117c35b33d89e6b82f6233ac2ab2
Nicholas Reid <nicholas.reid@ad.aie.edu>
2016-11-14 10:14:47 +1030

    Pedestal puzzle start

---
+++ Assets/Scripts/PedestalCheck1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PedestalCheck1 : MonoBehaviour
+{
+    public float checkLength = 20.0f;
+    private RaycastHit hit;
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.up , out hit , checkLength ))
+        {
+            if(hit.gameObject.tag == "toy1")
+            {
+                Debug.DrawRay(transform.position, Vector3.up , Color.green , checkLength);
+
+            }
+        }
+
+    }
+}
