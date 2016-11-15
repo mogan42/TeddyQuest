cdd6e5d9193d93ae5378c3a6789838ded631e682
Kaleb Jenkins <kaleb.jenkins@ad.aie.edu>
2016-11-15 16:29:00 +1030

    added scripts, putting puzzles together

---
+++ Assets/Scripts/VegStuff/Bridge.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour {
+
+    public GameObject cubeBall, activationColour, bridgeA;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == cubeBall)
+        {
+            cubeBall.layer = 1;
+            bridgeA.GetComponent<BoxCollider>().enabled = true;
+            bridgeA.GetComponent<MeshRenderer>().enabled = true;
+            cubeBall.tag = "Untagged";
+            activationColour.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == cubeBall)
+        {
+            cubeBall.layer = 7;
+            bridgeA.GetComponent<BoxCollider>().enabled = false;
+            bridgeA.GetComponent<MeshRenderer>().enabled = false;
+            cubeBall.tag = "box";
+            activationColour.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/VegStuff/BridgeActivation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeActivation : MonoBehaviour {
+
+    public GameObject cubeBall, activationColour, bridgeA;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == cubeBall)
+        {
+            cubeBall.layer = 1;
+            cubeBall.tag = "Untagged";
+            bridgeA.SetActive(true);
+            activationColour.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == cubeBall)
+        {
+            cubeBall.layer = 7;
+            cubeBall.tag = "box";
+            activationColour.SetActive(false);
+            bridgeA.SetActive(false);
+        }
+    }
+}
