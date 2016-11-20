5f9f03918c2cc5df5d5572d1f6faf1bcc3b1df1a
Morgans-PC\Morgan <mo.g@live.com.au>
2016-11-20 19:57:37 +1030

    scripting key pickups in my level

---
+++ Assets/Scripts/collectKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collectKey : MonoBehaviour {
+
+    public int keysToGive;
+    public GameObject player;
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider collided)
+    {
+        if (collided.tag == ("Player"))
+        {
+            KeyLog giveKey = player.GetComponent<KeyLog>();
+            giveKey.numberOfLevelKeys = keysToGive;
+            Destroy(gameObject);
+        }
+    }
+
+}
