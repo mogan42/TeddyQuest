3b8031560e779015dee78a7c4b8c61fbf4f9970b
Morgans-PC\Morgan <mo.g@live.com.au>
2016-11-20 22:54:23 +1030

    working on geting amazon library compleatable

--- Assets/Scripts/BlueKeyLockUnlock.cs
+++ Assets/Scripts/BlueKeyLockUnlock.cs
@@ -5,18 +5,19 @@ public class BlueKeyLockUnlock : MonoBehaviour {
 
     public GameObject player;
     public Animator lockAnim;
+    public KeyCode KeyToPress = KeyCode.E;
 
 
     void OnTriggerEnter(Collider collided)
     {
         KeyLog hasRightKey = player.GetComponent<KeyLog>();
-        if (player.tag == ("Player"))
-        {
-            if (hasRightKey.hasBlueKey == true)
-            {
+        Debug.Log("GetComponent");
+          if (Input.GetKeyDown(KeyToPress) && hasRightKey.hasBlueKey == true)
+           {
+            Debug.Log("play animation");
                 lockAnim.SetTrigger("Unlock");
-            }
-        }
-    } 
+           }
+
+       } 
 
 }
