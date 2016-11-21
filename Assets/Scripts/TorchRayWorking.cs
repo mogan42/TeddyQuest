d5f527bbec34275d28e4cd89208078d362c38515
Kaleb Jenkins <kaleb.jenkins@ad.aie.edu>
2016-11-21 16:24:19 +1030

    Added stuff to scene, missing: falling down restart, grabbing the key, ending game

--- Assets/Scripts/TorchRayWorking.cs
+++ Assets/Scripts/TorchRayWorking.cs
@@ -38,5 +38,4 @@ public class TorchRayWorking : MonoBehaviour
             }
         }
     }
-
 }
---
+++ Assets/Scripts/VegStuff/KeyGrabS.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyGrabS : MonoBehaviour {
+
+    public GameObject door;
+    public KeyCode KeyToPress = KeyCode.E;
+
+    void OnTriggerStay(Collider d)
+    {
+        if (d.tag == "Player" && Input.GetKeyDown(KeyToPress))
+        {
+            door.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
