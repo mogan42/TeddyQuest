80b1943a26c152b44f31b6b483ec4cbce49a9c4d
Morgan Davies <morgan.davies@ad.aie.edu>
2016-11-21 16:10:21 +1030

    pushing changes, did a lot of things. completed level and added particle prefab for kristian

---
+++ Assets/Scripts/GreenKeyLockUnlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreenKeyLockUnlock : MonoBehaviour {
+
+    public GameObject player;
+    public Animator lockAnim;
+    public KeyCode KeyToPress = KeyCode.E;
+
+
+    void OnTriggerStay(Collider collided)
+    {
+        KeyLog hasRightKey = player.GetComponent<KeyLog>();
+        //Debug.Log(hasRightKey.hasBlueKey);
+          if (Input.GetKeyDown(KeyToPress) && hasRightKey.hasGreenKey)
+           {
+            //Debug.Log("play animation");
+                lockAnim.SetTrigger("Unlock");
+           }
+
+       } 
+
+}
---
+++ Assets/Scripts/YellowKeyLockUnlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YellowKeyLockUnlock : MonoBehaviour {
+
+    public GameObject player;
+    public Animator lockAnim;
+    public KeyCode KeyToPress = KeyCode.E;
+
+
+    void OnTriggerStay(Collider collided)
+    {
+        KeyLog hasRightKey = player.GetComponent<KeyLog>();
+        //Debug.Log(hasRightKey.hasBlueKey);
+          if (Input.GetKeyDown(KeyToPress) && hasRightKey.hasYellowKey)
+           {
+            //Debug.Log("play animation");
+                lockAnim.SetTrigger("Unlock");
+           }
+
+       } 
+
+}
