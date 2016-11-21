581dc95646574420bd0a7a9c2f131b99661adcb8
Morgans-PC\Morgan <mo.g@live.com.au>
2016-11-22 01:05:40 +1030

    created a credits scene, added an ending to the game and addded audio to Veg Forest

--- Assets/Scripts/OpeningFinalDoor.cs
+++ Assets/Scripts/OpeningFinalDoor.cs
@@ -6,7 +6,10 @@ public class OpeningFinalDoor : MonoBehaviour
 
     public GlobalLog globalLog;
     public GameObject Lock1, Lock2, Lock3, Door;
-	void OnCollisionEnter()
+
+    public Animator padLock1, padLock2, padLock3, finalDoor;
+
+	void OnTriggerEnter(Collider collided)
     {
         int keys = globalLog.numberOfKeys;
         bool lock1 = globalLog.lock1Open, lock2 = globalLog.lock2Open, lock3 = globalLog.lock3Open;
@@ -16,6 +19,7 @@ public class OpeningFinalDoor : MonoBehaviour
             //play animation for unlocking the door.
             //use Lock 1 for that lock if needed.
             lock1 = true;
+            padLock1.SetTrigger("FinalDoorUnlock");
         }
         if (keys >1 &! lock2)
         {
@@ -23,19 +27,21 @@ public class OpeningFinalDoor : MonoBehaviour
             //use Lock 2 for that lock if needed.
 
             lock2 = true;
+            padLock2.SetTrigger("FinalDoorUnlock");
         }
         if (keys >2 & !lock3)
         {
             //play animation for unlocking the door.
             //use Lock 3 for that lock if needed.
-
+            padLock3.SetTrigger("FinalDoorUnlock");
             lock3 = true;
+            
         }
         if (lock3)
         {
             //play animation for door opening.
             //use Door for animation if needed.
-
+            finalDoor.SetTrigger("FinalDoorUnlock");
         }
     }
 }
